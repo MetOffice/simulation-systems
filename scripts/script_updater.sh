10cbff4d596822a3c15778a9266b5a85cdbf0375
Lucy Gordon <lucy.gordon@metoffice.gov.uk>
2023-07-05 11:11:06 +0100

    added script updater to github scripts folder

---
+++ scripts/script_updater.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+github_url="https://github.com/MetOffice/simulation-systems.git"
+
+
+# pulled_repo="simulation-systems"
+
+# tmp_dir=$(mktemp -d)
+
+echo "updater script has started running"
+
+rm -rf $HOME/Documents/um_scripts_git/pulled_repo_container
+
+git clone -b Lucy_testing --single-branch $github_url $HOME/Documents/um_scripts_git/pulled_repo_container
+
+# git clone $github_url $HOME/Documents/um_scripts_git/pulled_repo_container--branch UMDIR-Scripts --single-branch
+
+# git clone $github_url $tmp_dir
+
+# mv $tmp_dir/$pulled_repo $HOME/Documents/um_scripts_git/pulled_repo_container
+
+#echo "updater script finished running"
+
+#chmod +x $HOME/Documents/um_scripts_git/test.sh
+
+#chmod +x $HOME/Documents/um_scripts_git/pulled_repo_container/scripts/script_umdp3_checker/bin/umdp3_check.pl
+
+#./test.sh
+
+#./pulled_repo_container/scripts/script_umdp3_checker/bin/umdp3_check.pl
+
+# rm -rf $tmp_dir
+
+
