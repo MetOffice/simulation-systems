8a0b0a26eb3520f7be8eb5f6cdc645c3c20d01cc
Lucy Gordon <lucy.gordon@metoffice.gov.uk>
2023-07-18 12:17:14 +0100

    added error messages to script_updater

--- scripts/script_updater.sh
+++ scripts/script_updater.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -eu
+
 github_url="https://github.com/MetOffice/simulation-systems.git"
 
 
-echo "updater script has started running"
+echo "Git-scripts updater has started running"
 
-rm -rf $HOME/Documents/um_scripts_git/pulled_repo_container
+rm -rf $HOME/Documents/um_scripts_git/pulled_repo_container || echo "unable to remove current simulation-systems git directory"
 
-git clone -b Lucy_testing_um_scripts --single-branch $github_url $HOME/Documents/um_scripts_git/pulled_repo_container
+git clone -b Lucy_testing_um_scripts --single-branch $github_url $HOME/Documents/um_scripts_git/pulled_repo_container  || echo "Unable to clone remote git repo into specified location. Check git branch, git url, destination path, permissions and git access"
 
-
+echo "Github scripts updated"
