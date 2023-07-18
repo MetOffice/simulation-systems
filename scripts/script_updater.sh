c14680c50b5b533c82b71e61fc1cb557898230aa
Lucy Gordon <lucy.gordon@metoffice.gov.uk>
2023-07-18 14:18:59 +0100

    fixed globbing bug

--- scripts/script_updater.sh
+++ scripts/script_updater.sh
@@ -7,8 +7,8 @@ github_url="https://github.com/MetOffice/simulation-systems.git"
 
 echo "Git-scripts updater has started running"
 
-rm -rf $HOME/Documents/um_scripts_git/pulled_repo_container || echo "unable to remove current simulation-systems git directory"
+rm -rf "${HOME}/Documents/um_scripts_git/pulled_repo_container" || echo "unable to remove current simulation-systems git directory"
 
-git clone -b Lucy_testing_um_scripts --single-branch $github_url $HOME/Documents/um_scripts_git/pulled_repo_container  || echo "Unable to clone remote git repo into specified location. Check git branch, git url, destination path, permissions and git access"
+git clone -b Lucy_testing_um_scripts --single-branch $github_url "${HOME}/Documents/um_scripts_git/pulled_repo_container" || echo "Unable to clone remote git repo into specified location. Check git branch, git url, destination path, permissions and git access"
 
 echo "Github scripts updated"
