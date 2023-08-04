5c1d990e8e188ee28d658b98264c4bf58aa3734b
Lucy Gordon <lucy.gordon@metoffice.gov.uk>
2023-08-04 16:13:46 +0100

    modified error feedback, redirected cloning message feedback,changed destination of cloned scripts

--- scripts/script_updater.sh
+++ scripts/script_updater.sh
@@ -7,7 +7,7 @@ github_url="https://github.com/MetOffice/simulation-systems.git"
 
 echo "Git-scripts updater has started running"
 
-rm -rf "${HOME}/Documents/um_scripts_git/pulled_repo_container"
+rm -rf "${CYLC_SUITE_SHARE_DIR}/imported_github_scripts"
 if [[ $? != 0 ]]; then
     echo "Couldn't remove specified folder. Try checking permissions"
     exit 1
@@ -15,7 +15,7 @@ if [[ $? != 0 ]]; then
     echo "Successfully removed old simulation-systems git directory"
 fi
 
-git clone -b Lucy_testing_um_scripts --single-branch $github_url "${HOME}/Documents/um_scripts_git/pulled_repo_container"
+git clone -b Lucy_testing_um_scripts --single-branch $github_url "${CYLC_SUITE_SHARE_DIR}/imported_github_scripts" 2>&1
 if [[ $? != 0 ]]; then
     echo "Unable to clone remote git repo into specified location. Check git branch, git url, destination path, permissions and git access"
     exit 1
